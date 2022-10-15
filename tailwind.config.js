4bd9fe4b751b247473a8efd500fd7a228202c103
Ben Miller <bjmiller86@gmail.com>
2022-10-15 11:39:12 -0700

    Added tailwindcss to this branch. Started changing the list and single page templates to incorporate the tailwind calsses.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = 
+{
+  content: ["./content/**/*.{html, js}", "./layouts/**/*.{html, js}"],
+  theme: 
+  {
+    extend: {},
+  },
+  plugins: [],
+}
